8c4f1d0e77030a197f970da9eb3ecdc3ffb0edad
bourich1 <bourichanouar@gmail.com>
2025-05-03 14:18:10 +0100

    c1

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,12 +66,12 @@ const cars = [
     price: "350 DH",
   },
   {
-    image: "./assets/images/cupra-1/Cupra -1.jpg",
+    image: "./assets/images/Cupra-1/Cupra1.jpg",
     images: [
-      "./assets/images/cupra-1/Cupra -1.jpg",
-      "./assets/images/cupra-1/Cupra -2.jfif",
-      "./assets/images/cupra-1/Cupra -3.jfif",
-      "./assets/images/cupra-1/Cupra.jpg"
+      "./assets/images/Cupra-1/Cupra1.jpg",
+      "./assets/images/Cupra-1/Cupra2.jfif",
+      "./assets/images/Cupra-1/Cupra3.jfif",
+      "./assets/images/Cupra-1/Cupra.jpg"
     ],
     title: "Cupra",
     year: "2025",
